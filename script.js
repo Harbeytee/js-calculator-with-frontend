f91767fc391f83cd6eca59aadaa06153b18900c6
Abby <harbeyteejay@gmail.com>
2022-07-06 01:13:22 +0100

    shortened code

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ for( let i =0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function(e){
     const button = e.target;
     const displayNum = display.textContent;
-    // checking if the display ends with an invalid input
-    var endsWith = /(([\W]|\x)[=])$/
     if (displayNum === '0'|| displayNum === 'syntax error' || displayNum === 'undefined'|| displayNum === 'Infinity' ) {
       display.textContent = button.textContent;
     }
@@ -20,60 +18,52 @@ for( let i =0; i < buttons.length; i++) {
       display.textContent = '0';
 
     }
-    if (e.target.innerHTML == '=' && endsWith.test(display.innerHTML) == false) {
+
+    if (e.target.innerHTML == '='/*&& endsWith.test(display.innerHTML) == false)*/) {
       let user = display.innerHTML.split(/[^0-9+.x%^÷-]/);  
       let users = user.slice(0, -1).join(" ");
       let answer;
-      if ((/([-]{2,})/g).test(users)) {
-        let new1 = users.replace(/([-]{2,})/g, '+');
-        
-        answer = Function("return" +  ` ${new1} `) ();
-        display.innerHTML = answer;
+      if ((/([-][-])/g).test(users)) {
+        let new1 = users.replace(/([-][-])/g, '+');
+        validate(new1)
       }
-      
       else if((/([+]{2,})/g).test(users)) {
-        let new1 = users.replace(/([+]{2,})/g, '+').slice(-1);
-        
-        answer = Function("return" +  ` ${new1} `) ();
-        display.innerHTML = answer;
-          
+        let new1 = users.replace(/([+]{2,})/g, '+');
+        validate(new1);
+        console.log(answer);
       }
-      else if(users.includes('÷÷') |users.includes('xx') |users.includes('^^') |users.includes('%%')) {
-      
+      else if(users.includes('÷÷') |users.includes('xx')) /*|users.includes('^^') |users.includes('%%'))*/ {
         answer = 'syntax error';
         display.innerHTML = answer;
-        
-      } 
-
+      }
       else if(users.includes('÷')) {
         let new1 = users.replace(/\÷/g, '/');
-        
-         answer = Function("return" +  ` ${new1} `) ();
-        
-        display.innerHTML = answer;
+        validate(new1)
         
       }
       else if(users.includes('x')) {
         let new1 = users.replace(/\x/g, '*');
-        answer = Function("return" +  ` ${new1} `) ();
-        display.innerHTML = answer;
+        validate(new1)
         
       }
       else if(users.includes('^')) {
         let new1 = users.replace(/\^/g, '**');
-        answer = Function("return" +  ` ${new1} `) ();
-        display.innerHTML = answer;
+        validate(new1)
         
       }
-
-     else {
-        answer = Function("return" +  ` ${users} `) ();
-        display.innerHTML = answer;
+      else {
+        validate(users);
       }
     }
-    else if(e.target.innerHTML == '='&& endsWith.test(display.innerHTML) == true){
-      
-      display.innerHTML = 'syntax error'
+    
+    function validate(prop) {
+      try {
+        let answer = Function("return" +  ` ${prop} ` ) ();
+        display.innerHTML = answer;
+      }
+      catch(err) {
+        display.innerHTML = 'syntax error';
+      }
     }
   });
 }
